8edfcc7f6c6bb8c049c824fb5e7b6fd838147cac
Justin Weiss <justin@justinweiss.com>
2021-06-01 11:35:04 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Trello Card Importer
+
+This [Aha! Develop](https://www.aha.io/develop/overview) extension allows you to bring Trello cards into Aha!. It currently supports:
+
+- Description (as HTML)
+- Checklists (as Aha! requirements with to-dos for each item)
+
+Coming soon:
+
+- Labels (as Aha! tags)
+- Attachments
+- Comments
+
+It provides the aha-develop.trello-import.cards [importer contribution](https://www.aha.io/support/develop/importers).
+
+## Demo
+
+TODO: Add a short .gif screen recording showing some of the highlights of your extension. This can be recorded using a tool like [LICECap](https://www.cockos.com/licecap/).
+
+![demo](demo.gif)
+
+## Installing the extension
+
+**Note: In order to install an extension into your Aha! Develop account, you must be an account administrator.**
+
+Install the Trello import extension by clicking [here](https://secure.aha.io/settings/account/extensions/install?url=https%3A%2F%2Fsecure.aha.io%2Fextensions%2Faha-develop.trello-import.gz).
+
+## Working on the extension
+
+Install [`aha-cli`](https://github.com/aha-app/aha-cli):
+
+```sh
+npm install -g aha-cli
+```
+
+Clone the repo:
+
+```sh
+git clone https://github.com/aha-develop/trello-import.git
+```
+
+**Note: In order to install an extension into your Aha! Develop account, you must be an account administrator.**
+
+Install the extension into Aha! and set up a watcher:
+
+```sh
+aha extension:install
+aha extension:watch
+```
+
+Now, any change you make inside your working copy will automatically take effect in your Aha! account.
+
+## Building
+
+When you have finished working on your extension, package it into a `.gz` file so that others can install it:
+
+```sh
+aha extension:build
+```
+
+After building, you can upload the `.gz` file to a publicly accessible URL, such as a GitHub release, so that others can install it using that URL.
+
+To learn more about developing Aha! Develop extensions, including the API reference, the full documentation is located here: [Aha! Develop Extension API](https://www.aha.io/support/develop/extensions)
---
+++ src/importers/cards.js
@@ -0,0 +1,143 @@
+import React from "https://cdn.skypack.dev/react";
+import marked from "https://cdn.skypack.dev/marked";
+
+const importer = aha.getImporter("aha-develop.trello-import.cards");
+
+async function trelloGet(path) {
+  const auth = await aha.auth("trello", { useCachedRetry: true });
+
+  const init = {
+    method: "GET",
+    headers: {
+      Authorization: `OAuth oauth_consumer_key="${auth.key}", oauth_token="${auth.token}"`,
+    },
+  };
+
+  const request = new Request(`https://api.trello.com/1${path}`, init);
+
+  return fetch(request).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Trello API error: ${response.status}`);
+    }
+
+    return response.json();
+  });
+}
+
+importer.on(
+  { action: "listCandidates" },
+  async ({ filters, nextPage }, { identifier, settings }) => {
+    if (!filters.board) return { records: [] };
+
+    const cards = await trelloGet(`/boards/${filters.board}/cards`);
+    const lists = await trelloGet(`/boards/${filters.board}/lists`);
+
+    const listsById = Object.fromEntries(lists.map((l) => [l.id, l]));
+
+    return {
+      records: cards.map((card) => ({
+        uniqueId: card.id,
+        list: listsById[card.idList]?.name,
+        name: card.name,
+        url: card.url,
+        description: card.desc,
+      })),
+    };
+  }
+);
+
+importer.on({ action: "listFilters" }, async ({}, { identifier, settings }) => {
+  return {
+    board: {
+      title: "Board",
+      required: true,
+      type: "select",
+    },
+  };
+});
+
+importer.on(
+  { action: "filterValues" },
+  async ({ filterName, filters }, { identifier, settings }) => {
+    let values = [];
+    switch (filterName) {
+      case "board":
+        const boards = await trelloGet("/members/me/boards?fields=name,id");
+        values = boards.map((b) => ({ text: b.name, value: b.id }));
+        break;
+      default:
+        break;
+    }
+    return values;
+  }
+);
+
+importer.on(
+  { action: "renderRecord" },
+  ({ record, onUnmounted }, { identifier, settings }) => {
+    return (
+      <div>
+        <span className="text-muted">{record.list}</span>
+        <br />
+        <a href={aha.sanitizeUrl(record.url)} target="_blank" rel="noopener">
+          {record.name}
+        </a>
+      </div>
+    );
+  }
+);
+
+importer.on(
+  { action: "importRecord" },
+  async ({ importRecord, ahaRecord }, { identifier, settings }) => {
+    let success = false;
+
+    ahaRecord.description =
+      marked(importRecord.description) +
+      `<p><a href='${aha.sanitizeUrl(
+        importRecord.url
+      )}'>View on Trello</a></p>`;
+
+    // Create a requirement with tasks for each checklist
+    const checklists = await trelloGet(
+      `/cards/${importRecord.uniqueId}/checklists`
+    );
+
+    for (const list of checklists) {
+      const requirement = new aha.models.Requirement({
+        name: list.name,
+        feature: ahaRecord,
+      });
+      success = await requirement.save({
+        args: {
+          skipRequiredFieldsValidation: true,
+        },
+      });
+
+      if (!success) return false;
+
+      const tasks = list.checkItems.map((item, i) => {
+        const status =
+          item.state === "complete"
+            ? aha.enums.TaskStatusEnum.COMPLETE
+            : aha.enums.TaskStatusEnum.PENDING;
+
+        return new aha.models.Task({
+          name: item.name,
+          body: "",
+          position: i + 1,
+          record: requirement,
+          status: status,
+          dueDate: item.due,
+        });
+      });
+
+      for (const task of tasks) {
+        success = await task.save();
+        if (!success) return false;
+      }
+    }
+
+    return ahaRecord.save();
+  }
+);
